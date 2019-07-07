678e1e8f85adcef619d76359e5bbef5f6ee01595
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2019-07-07 18:59:31 +0200

    Create 2.18.c

---
+++ 2.18.c
@@ -0,0 +1,43 @@
+/*
+
+2.18 (Comparing Values)
+Write a program that asks the user to enter the highest
+rainfall ever in one season for a country, and the rainfall in the current year
+for that country, obtains the values from the user, checks if the current
+rainfall exceed the highest rainfall and prints an appropriate message on the
+screen. If the current rainfall is higher, it assigns
+that value as the highest rainfall ever. Use only the single-selection form of
+the if statement you learned in this chapter.
+
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  float Current_Rainfall,
+
+        Highest_Rainfall,
+
+        Grow_Rate;
+
+    printf("%s","Please enter the highest rainfall in one season: ");
+
+    scanf("%f", &Highest_Rainfall);
+
+    printf("%s","\nPlease enter rainfall in the current year: ");
+
+    scanf("%f", &Current_Rainfall);
+
+    Grow_Rate = (( Current_Rainfall - Highest_Rainfall ) / Highest_Rainfall ) * 100;
+
+    if(  Grow_Rate >= 1 && Grow_Rate <= 50  )
+          Highest_Rainfall = Current_Rainfall;
+
+    if ( Grow_Rate >= 51 && Grow_Rate < 100 )
+          puts("\nThis rainfall exceed the highest rainfall in this year!");
+
+
+}
