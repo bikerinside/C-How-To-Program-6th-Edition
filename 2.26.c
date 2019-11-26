31c9ec1a31207ea071a142733e5f30b27306e2c7
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2019-11-26 18:59:35 +0100

    Update 2.26.c

--- 2.26.c
+++ 2.26.c
@@ -1,1 +1,25 @@
+/*
 
+2.26 (Multiples)
+Write a program that reads in two integers and determines and prints whether
+the first is a multiple of the second. [Hint: Use the remainder operator.]
+
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  int x, y;
+
+  printf("%s", "\nInsert two integers : "); scanf("%d%d", &x, &y);
+
+  if( ( x % y ) == 0 ) printf("\n%d is a multiple of %d\n\n", x, y);
+
+  if( ( x % y ) != 0 ) printf("\n%d is not a multiple of %d\n\n", x, y);
+
+  return 0;
+
+}
