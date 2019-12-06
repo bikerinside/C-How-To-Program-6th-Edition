ad3ac7e6141fe0d16d604998dd7afa49d9d7c782
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2019-12-06 19:23:36 +0100

    Add files via upload

---
+++ src/Chapter 3/3.39.c
@@ -0,0 +1,55 @@
+/*
+
+3.39
+
+(Counting 7s) Write a program that reads an integer and determines and prints how many
+digits in the integer are 7s.
+
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  int Num           = 0,  /* here is stored number inserted by user */
+      NumCopy       = 0,  /* here is stored a copy of number inserted */
+      NumDig        = 0,  /* here is stored single digit of number */
+      Div           = 1,  /* in Div var is stored the value that divide  number to obtain single digit */
+      Counter       = 0,  /* Counter store the number of digit obtanied from inserted  number */
+      CounterSeven  = 0;
+
+  printf("\nInsert number: "); scanf("%d", &Num);
+  /* Need a copy of number inserted by user */
+  NumCopy = Num;
+  /* The first while-loop count how many digits, the number inserted by user has */
+   while( NumCopy > 0 )
+  /* this while loop will stop when division between NumCopy and 10 is egual to 0 */
+   {/* means that digits are over */
+
+     NumCopy /= 10;
+    /* Counter count digits of Number */
+     Counter++;
+
+   } /* end of the first while-loop */
+
+  while( Counter > 0 )
+  /* while-loop ends when Counter is equal to 0 that means that there are no more digits to check */
+  {
+      /* Num will be divided by 1,10,100... and than modulo operation will calculate the single digit  */
+      NumDig = ( Num / Div ) % 10;
+      /* CounterSeven is postincremented if the digit store in NumDig is equal to 7 */
+      if(NumDig == 7) CounterSeven++;
+      /* same but by 10 for var Div */
+      Div *= 10;
+      /* Counter is decremented because digit has been checked by if-case */
+      Counter--;
+
+  }
+
+  printf("Number of 7 found in %d = %d\n", Num, CounterSeven);
+
+  return 0;
+
+}
