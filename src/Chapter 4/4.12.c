f257d72600f07a0e2afb95a9cd625382faee8251
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2019-12-19 18:28:59 +0100

    Add files via upload

---
+++ src/Chapter 4/4.12.c
@@ -0,0 +1,30 @@
+/*
+4.12
+
+(Prime Numbers) Write a program to calculate and print a list of all prime numbers from
+1 to 100.
+
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  for(int i = 1; i <= 100; i += 2) printf("\n%d", i);
+
+  return 0;
+
+}
+
+/**************************************************************************
+ * (C) Copyright 2019 by bikerinside                                      *
+ *                                                                        *
+ * DISCLAIMER:                                                            *
+ * The author make no warranty of any kind, expressed or implied,         *
+ * with regard to these programs.                                         *
+ * The author shall not be liable in any event for incidental or          *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
