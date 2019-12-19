92e92b48f4608063d2faec76e1d1c4dfae72a6d7
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2019-12-19 18:23:21 +0100

    Add files via upload

---
+++ src/Chapter 4/4.11.c
@@ -0,0 +1,31 @@
+/*
+
+4.11 
+
+(Calculating the Sum of Multiples) Write a program to calculate and print the sum of all
+multiples of 7 from 1 to 100.
+
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  for(int i = 1; i <= 100; i++ ) 
+    printf("\nn = %d\t%d", i, i*7);
+
+}
+
+
+/**************************************************************************
+ * (C) Copyright 2019 by bikerinside                                      *
+ *                                                                        *
+ * DISCLAIMER:                                                            *
+ * The author make no warranty of any kind, expressed or implied,         *
+ * with regard to these programs.                                         *
+ * The author shall not be liable in any event for incidental or          *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
