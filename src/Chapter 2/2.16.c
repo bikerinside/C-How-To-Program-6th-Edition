36142186ac2e884276d6b1520de4914e1774f560
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2019-12-20 09:35:28 +0100

    Update 2.16.c

--- src/Chapter 2/2.16.c
+++ src/Chapter 2/2.16.c
@@ -42,3 +42,14 @@ int main()
  you should never use printf() only with one argument.
 
 */
+
+/**************************************************************************
+ * (C) Copyright 2019 by bikerinside                                      *
+ *                                                                        *
+ * DISCLAIMER:                                                            *
+ * The author make no warranty of any kind, expressed or implied,         *
+ * with regard to these programs.                                         *
+ * The author shall not be liable in any event for incidental or          *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
