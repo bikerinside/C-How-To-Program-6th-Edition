d3a0a873a9ae5fac96929cbf4911f19c5a7fcc59
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2019-12-20 09:53:13 +0100

    Update 3.49b.c

--- src/Chapter 3/3.49b.c
+++ src/Chapter 3/3.49b.c
@@ -68,3 +68,14 @@ int main()
     else printf("\nDecrypted code = %d\n", UserCode);
 
 }
+
+/**************************************************************************
+ * (C) Copyright 2019 by bikerinside                                      *
+ *                                                                        *
+ * DISCLAIMER:                                                            *
+ * The author make no warranty of any kind, expressed or implied,         *
+ * with regard to these programs.                                         *
+ * The author shall not be liable in any event for incidental or          *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
